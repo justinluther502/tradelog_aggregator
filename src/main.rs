c1ca69d7fb895da384a20f69d1e9f02ddf715dff
Justin Luther <justinluther@gmail.com>
2022-08-12 23:49:52 -0400

    lists all csv files now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use glob::glob;
+use std::path::PathBuf;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut searchstr = env::current_dir().unwrap().into_os_string().into_string().unwrap();
+    searchstr.push_str("/*.csv");
+    let file_list = list_csvs(&searchstr);
+    println!("{:?}", file_list);
+}
+
+fn list_csvs(filepath: &str) -> Vec<PathBuf> {
+    let mut csv_list = Vec::new();
+    for entry in glob(filepath).expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => csv_list.push(path),
+            Err(e) => println!("{:?}", e),
+        }
+    }
+    csv_list
 }
