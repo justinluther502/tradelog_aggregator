0fac2ef8df8156865022ff1e6a1a8d6c0272333a
Justin Luther <justinluther@gmail.com>
2022-08-13 00:04:58 -0400

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 use glob::glob;
-use std::path::PathBuf;
 use std::env;
 
 fn main() {
-    let mut searchstr = env::current_dir().unwrap().into_os_string().into_string().unwrap();
+    let mut searchstr = env::current_dir()
+        .unwrap()
+        .into_os_string()
+        .into_string()
+        .unwrap();
     searchstr.push_str("/*.csv");
     let file_list = list_csvs(&searchstr);
     println!("{:?}", file_list);
 }
 
-fn list_csvs(filepath: &str) -> Vec<PathBuf> {
+fn list_csvs(filepath: &str) -> Vec<String> {
     let mut csv_list = Vec::new();
     for entry in glob(filepath).expect("Failed to read glob pattern") {
         match entry {
-            Ok(path) => csv_list.push(path),
+            Ok(path) => csv_list.push(path.into_os_string().into_string().unwrap()),
             Err(e) => println!("{:?}", e),
         }
     }
