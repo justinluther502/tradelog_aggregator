747bc180008c7397ddfbfb035c92e31de1c7ff14
Justin Luther <justinluther@gmail.com>
2022-08-13 19:48:21 -0400

    works but needs same columns for each file

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use glob::glob;
-use std::env;
+// use std::env;
+use std::{error::Error, str::FromStr};
 use csv::{Reader, StringRecord, Writer};
+use chrono;
 
 fn main() {
-    let mut searchstr = env::current_dir()
-        .unwrap()
-        .into_os_string()
-        .into_string()
-        .unwrap();
-    searchstr.push_str("/*.csv");
+    // let mut searchstr = env::current_dir()
+    //     .unwrap()
+    //     .into_os_string()
+    //     .into_string()
+    //     .unwrap();
+    // searchstr.push_str("/*.csv");
+    let searchstr = "C:\\Users\\justi\\Documents\\LWM\\7. Trade Logs/*.csv";
     let file_list = list_csvs(&searchstr);
     let rows = read_all_csvs(file_list);
-    write_combined_csv(rows);
+    write_combined_csv(rows).unwrap();
 }
 
 fn list_csvs(searchstr: &str) -> Vec<String> {
@@ -29,10 +32,7 @@ fn read_file(filepath: &str) -> Vec<StringRecord> {
     let mut rdr = Reader::from_path(filepath).unwrap();
     let mut rows = Vec::new();
     for record in rdr.records() {
-        let mut row = record.unwrap();
-        // Truncate to 17 columns as a hacky way to fix reader reading extra blank "" values on
-        // right edge of CSV.
-        row.truncate(17);
+        let row = record.unwrap();
         rows.push(row);
     }
     rows
@@ -51,10 +51,14 @@ fn read_all_csvs(files: Vec<String>) -> Vec<StringRecord> {
     rows    
 }
 
-fn write_combined_csv(rows: Vec<StringRecord>) {
-    let mut wtr = Writer::from_path("consolidated.csv").unwrap();
+fn write_combined_csv(rows: Vec<StringRecord>) -> Result<(), Box<dyn Error>> {
+    let mut filename = String::from_str("Consolidated Logs - ")?;
+    filename.push_str(&chrono::offset::Local::now().to_string()[..10]);
+    filename.push_str(".csv");
+    let mut wtr = Writer::from_path(filename)?;
     for row in rows {
-        wtr.write_record(&row).unwrap();
+        wtr.write_record(&row)?;
     }
-    wtr.flush().unwrap();
+    wtr.flush()?;
+    Ok(())
 }
