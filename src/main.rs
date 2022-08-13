19047d796481288a998d1465e643a7a677496b14
Justin Luther <justinluther@gmail.com>
2022-08-13 13:14:15 -0400

    kind of works but not all rows.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use glob::glob;
 use std::env;
+use csv::{Reader, StringRecord, Writer};
 
 fn main() {
     let mut searchstr = env::current_dir()
@@ -9,16 +10,48 @@ fn main() {
         .unwrap();
     searchstr.push_str("/*.csv");
     let file_list = list_csvs(&searchstr);
-    println!("{:?}", file_list);
+    let rows = read_all_csvs(file_list);
+    write_combined_csv(rows);
 }
 
-fn list_csvs(filepath: &str) -> Vec<String> {
+fn list_csvs(searchstr: &str) -> Vec<String> {
     let mut csv_list = Vec::new();
-    for entry in glob(filepath).expect("Failed to read glob pattern") {
+    for entry in glob(searchstr).expect("Failed to read glob pattern") {
         match entry {
             Ok(path) => csv_list.push(path.into_os_string().into_string().unwrap()),
             Err(e) => println!("{:?}", e),
         }
     }
     csv_list
+}
+
+fn read_file(filepath: &str) -> Vec<StringRecord> {
+    let mut rdr = Reader::from_path(filepath).unwrap();
+    let mut rows = Vec::new();
+    for result in rdr.records() {
+        let record = result.unwrap();
+        rows.push(record);
+    }
+    rows
+}
+
+fn read_all_csvs(files: Vec<String>) -> Vec<StringRecord> {
+    let mut rows = Vec::new();
+    let mut rdr = Reader::from_path(&files[0]).unwrap();
+    let headers = rdr.headers().unwrap().to_owned();
+    rows.push(headers);
+
+    for file in files {
+        let mut new_rows = read_file(&file);
+        rows.append(&mut new_rows);
+    }
+    rows    
+}
+
+fn write_combined_csv(rows: Vec<StringRecord>) {
+    let mut wtr = Writer::from_path("consolidated.csv").unwrap();
+    for row in rows {
+        wtr.write_record(&row);
+    }
+    wtr.flush();
 }
