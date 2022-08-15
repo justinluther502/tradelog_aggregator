3eeb2d26fed445f6788b133b420c0cf3810cf1cd
Justin Luther <justinluther@gmail.com>
2022-08-15 13:32:52 -0400

    couple doc comments

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,10 @@ fn main() {
 }
 
 fn list_csvs(searchpath: PathBuf) -> Vec<PathBuf> {
+    // Lists all CSV files in the input search path.
     let mut csv_list = Vec::new();
     let searchstr = searchpath.to_str().unwrap();
+
     for entry in glob(searchstr).expect("Failed to read glob pattern") {
         match entry {
             Ok(path) => csv_list.push(path),
@@ -25,33 +27,38 @@ fn list_csvs(searchpath: PathBuf) -> Vec<PathBuf> {
 }
 
 fn read_file(filepath: PathBuf) -> Vec<StringRecord> {
+    // Reads CSV at filepath and returns a vector of row records.
     let mut rdr = Reader::from_path(filepath).unwrap();
-    let mut rows = Vec::new();
+    let mut file_rows = Vec::new();
+
     for record in rdr.records() {
         let row = record.unwrap();
-        rows.push(row);
+        file_rows.push(row);
     }
-    rows
+    file_rows
 }
 
 fn read_all_csvs(files: Vec<PathBuf>) -> Vec<StringRecord> {
-    let mut rows = Vec::new();
+    // Reads all CSVs in a vector and returns a vector of aggregated row records.
+    let mut combined_rows = Vec::new();
     let mut rdr = Reader::from_path(&files[0]).unwrap();
     let headers = rdr.headers().unwrap().to_owned();
-    rows.push(headers);
+    combined_rows.push(headers);
 
     for file in files {
         let mut new_rows = read_file(file);
-        rows.append(&mut new_rows);
+        combined_rows.append(&mut new_rows);
     }
-    rows
+    combined_rows
 }
 
 fn write_combined_csv(rows: Vec<StringRecord>) -> Result<(), Box<dyn Error>> {
+    // Takes a vector of row records and writes a CSV with filename encoded by today's date.
     let mut filename = String::from_str("Consolidated Logs - ")?;
     filename.push_str(&chrono::offset::Local::now().to_string()[..10]);
     filename.push_str(".csv");
     let mut wtr = Writer::from_path(filename)?;
+
     for row in rows {
         wtr.write_record(&row)?;
     }
